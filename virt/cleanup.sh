cbe2343e92272eaeaf2b671fed00d17beb61650d
Joshua Powers <josh.powers@canonical.com>
2018-03-28 13:12:45 -0700

    removing a grep -v

--- virt/cleanup.sh
+++ virt/cleanup.sh
@@ -4,8 +4,8 @@ set -ux
 PREFIX="testkvm"
 
 lxc list | grep "$PREFIX"
-lxc list -c n --format csv | grep -v "$PREFIX" | xargs lxc stop
-lxc list -c n --format csv | grep -v "$PREFIX" | xargs lxc delete
+lxc list -c n --format csv | grep "$PREFIX" | xargs lxc stop
+lxc list -c n --format csv | grep "$PREFIX" | xargs lxc delete
 rm /tmp/qemu-libvirt-test.sh.lock
 
 exit 0
