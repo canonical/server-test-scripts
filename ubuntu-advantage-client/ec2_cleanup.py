458611534b6701ffe54fb825ad0f0c261c462e38
Chad Smith <chad.smith@canonical.com>
2020-08-10 12:02:49 -0600

    ec2_cleanup: parametrize filter tag

--- ubuntu-advantage-client/ec2_cleanup.py
+++ ubuntu-advantage-client/ec2_cleanup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
-"""Clean up all running EC2 instances, VPCs, and storage.
+"""Clean up all running EC2 instances, VPCs, and storage."""
 
-Copyright 2018 Canonical Ltd.
-Joshua Powers <josh.powers@canonical.com>
-"""
+# Copyright 2018 Canonical Ltd.
+# Joshua Powers <josh.powers@canonical.com>
+
 import boto3
 
+import argparse
 
-CI_TAG = "uaclient-*"
+
+CI_DEFAULT_TAG = "uaclient-*"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--tag", dest="tag", action="store", default=CI_DEFAULT_TAG,
+        help=("Tag used to filter cloud resources for deletion. "
+              "Default: {}".format(CI_DEFAULT_TAG))
+    )
+    return parser.parse_args()
+
 
 def clean_ec2(tag):
     """Clean up all running EC2 instances, VPCs, and storage."""
     client = boto3.client('ec2')
     resource = boto3.resource('ec2')
 
-    print('# searching for vpcs')
+    print('# searching for vpcs matching tag {}'.format(tag))
     tag_filter = [{'Name': 'tag:Name', 'Values': [tag]}]
     for vpc in list(resource.vpcs.filter(Filters=tag_filter)):
         print('cleaning up vpc %s' % vpc.id)
@@ -43,22 +56,23 @@ def clean_ec2(tag):
         print('terminating vpc %s' % vpc.id)
         vpc.delete()
 
-    print('# searching for ssh keys')
-    key_name = {'Name': 'key-name', 'Values': [CI_TAG]}
+    print('# searching for ssh keys matching tag {}'.format(tag))
+    key_name = {'Name': 'key-name', 'Values': [tag]}
     for key in client.describe_key_pairs(Filters=[key_name])['KeyPairs']:
         print('deleting ssh key %s' % key['KeyName'])
         client.delete_key_pair(KeyName=key['KeyName'])
 
-    print('# searching for amis')
+    print('# searching for amis matching tag {}'.format(tag))
     for image in resource.images.filter(Owners=['self']).all():
         print('removing custom ami %s' % image.id)
         client.deregister_image(ImageId=image.id)
 
-    print('# searching for snapshots')
+    print('# searching for snapshots matching tag {}'.format(tag))
     for snapshot in resource.snapshots.filter(OwnerIds=['self']).all():
         print('removing custom snapshot %s' % snapshot.id)
         client.delete_snapshot(SnapshotId=snapshot.id)
 
 
 if __name__ == '__main__':
-    clean_ec2(CI_TAG)
+    args = parse_args()
+    clean_ec2(args.tag)
