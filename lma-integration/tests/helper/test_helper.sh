92486f55b94b2766e9720ea0caad1a7d0ac1b90e
Andreas Hasenack <andreas@canonical.com>
2020-10-22 18:14:07 -0300

    Move unit tests to a new directory, outside of lma-integration

--- lma-integration/tests/helper/test_helper.sh
+++ lma-integration/tests/helper/test_helper.sh
@@ -9,55 +9,6 @@ load_shunit2() {
   fi
 }
 
-debug() {
-    if [ -n "${DEBUG_TESTS}" ]; then
-        if [ "${1}" = "-n" ]; then
-            shift
-            echo -n "$@"
-        else
-            echo "$@"
-        fi
-    fi
-}
-
-stop_container_sync() {
-    local id=${1}
-    local timeout=10
-    max=$timeout
-    docker container stop ${id} > /dev/null 2>&1
-    while docker container ls --no-trunc 2>&1 | grep -q "${id}"; do
-        sleep 1
-        timeout=$(($timeout-1))
-        if [ "$timeout" -le 0 ]; then
-            fail "ERROR, failed to stop container ${id} in ${max} seconds"
-        fi
-    done
-}
-
-# $1: container id
-# $2: last message to look for in logs
-wait_container_ready() {
-    local id="${1}"
-    local msg="${2}"
-	local timeout=10
-    local logs
-
-	max=${timeout}
-    debug -n "Waiting for container to be ready "
-    logs=$(docker logs ${id} 2>&1 | tail -n 1)
-    while ! echo "${logs}" | grep -q "${msg}"; do
-        debug -n "."
-        sleep 1
-		timeout=$(($timeout-1))
-        if [ "${timeout}" -le 0 ]; then
-            fail "ERROR, failed to start container ${id} in ${max} seconds"
-        fi
-        logs=$(docker logs ${id} 2>&1 | tail -n 1)
-    done
-    debug
-}
-
-
 # export some global variables
 export prometheus_url="http://127.0.0.1:9090"
 export alertmanager_url="http://127.0.0.1:9093"
