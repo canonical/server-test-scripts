b5aca06e3ec91780eaeef7d82bc6c3810caec311
Paride Legovini <paride.legovini@canonical.com>
2020-11-09 18:11:23 +0100

    oci: add more nginx unit tests
    Added:
     - test_default_config_ipv6
     - test_static_files
     - test_static_files_read_only_mode
     - test_custom_config

--- oci-unit-tests/nginx_test.sh
+++ oci-unit-tests/nginx_test.sh
@@ -1,4 +1,6 @@
-. $(dirname $0)/helper/test_helper.sh
+# shellcheck shell=dash
+
+. "$(dirname "$0")/helper/test_helper.sh"
 
 # cheat sheet:
 #  assertTrue $?
@@ -55,6 +57,62 @@ test_default_config() {
     wait_nginx_container_ready "${container}" || return 1
 
     assertTrue "curl -sS http://127.0.0.1:48080 | grep -Fq 'Welcome to nginx!'"
+}
+
+test_default_config_ipv6() {
+    debug "Creating all-defaults nginx container"
+    container=$(docker_run_server -p 48080:80)
+
+    assertNotNull "Failed to start the container" "${container}" || return 1
+    wait_nginx_container_ready "${container}" || return 1
+
+    assertTrue "curl -sS http://[::1]:48080 | grep -Fq 'Welcome to nginx!'"
+}
+
+test_static_files() {
+    debug "Creating container with known static files"
+    test_data_wwwroot="$PWD/nginx_test_data/html"
+    container=$(docker_run_server -p 48080:80 -v "$test_data_wwwroot:/var/www/html:ro")
+
+    assertNotNull "Failed to start the container" "${container}" || return 1
+    wait_nginx_container_ready "${container}" || return 1
+
+    orig_checksum=$(md5sum "$test_data_wwwroot/test.txt" | awk '{ print $1 }')
+    retrieved_checksum=$(curl -sS http://127.0.0.1:48080/test.txt | md5sum | awk '{ print $1 }')
+
+    assertEquals "${orig_checksum}" "${retrieved_checksum}"
+}
+
+test_static_files_read_only_mode() {
+    debug "Creating container with known static files"
+    test_data_wwwroot="$PWD/nginx_test_data/html"
+    nginx_scratch=$(mktemp -d /tmp/nginx-cache-XXXXXXXX)
+    container=$(docker_run_server -p 48080:80 --read-only -v "$nginx_scratch:/var/lib/nginx" -v "$nginx_scratch:/var/log/nginx" -v "$nginx_scratch:/var/run" -v "$test_data_wwwroot:/var/www/html:ro")
+
+    assertNotNull "Failed to start the container" "${container}" || return 1
+    wait_nginx_container_ready "${container}" || return 1
+
+    orig_checksum=$(md5sum "$test_data_wwwroot/test.txt" | awk '{ print $1 }')
+    retrieved_checksum=$(curl -sS http://127.0.0.1:48080/test.txt | md5sum | awk '{ print $1 }')
+
+    assertEquals "${orig_checksum}" "${retrieved_checksum}"
+
+    rm -rf "$nginx_scratch"
+}
+
+test_custom_config() {
+    debug "Creating container with custom config"
+    custom_config="$PWD/nginx_test_data/nginx.conf"
+    test_data_wwwroot="$PWD/nginx_test_data/html"
+    container=$(docker_run_server -p 48080:80 -v "$custom_config:/etc/nginx/nginx.conf:ro" -v "$test_data_wwwroot:/srv/www/html:ro")
+
+    assertNotNull "Failed to start the container" "${container}" || return 1
+    wait_nginx_container_ready "${container}" || return 1
+
+    orig_checksum=$(md5sum "$test_data_wwwroot/index.html" | awk '{ print $1 }')
+    retrieved_checksum=$(curl -sS http://127.0.0.1:48080/ | md5sum | awk '{ print $1 }')
+
+    assertEquals "${orig_checksum}" "${retrieved_checksum}"
 }
 
 load_shunit2
