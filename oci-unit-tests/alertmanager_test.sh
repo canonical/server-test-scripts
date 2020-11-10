4154bed0c16e34a49edd036023073113f60c5aec
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2020-11-10 15:17:40 -0300

    OCI unit test: alertmanager
    A fired alert scenario was tested, more test cases should be added.

---
+++ oci-unit-tests/alertmanager_test.sh
@@ -0,0 +1,82 @@
+# shellcheck shell=dash
+
+# shellcheck disable=SC1090
+. "$(dirname "$0")/helper/test_helper.sh"
+
+# cheat sheet:
+#  assertTrue $?
+#  assertEquals 1 2
+#  oneTimeSetUp()
+#  oneTimeTearDown()
+#  setUp() - run before each test
+#  tearDown() - run after each test
+
+# The name of the temporary docker network we will create for the
+# tests.
+readonly DOCKER_PREFIX=oci_alertmanager_test
+readonly DOCKER_IMAGE="squeakywheel/prometheus-alertmanager:edge"
+readonly ALERTMANAGER_PORT=60001
+
+oneTimeSetUp() {
+    # Cleanup stale resources
+    tearDown
+}
+
+setUp() {
+    suffix=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 8 | head -n 1)
+}
+
+tearDown() {
+    for container in $(docker ps -a --filter "name=$DOCKER_PREFIX" --format "{{.Names}}"); do
+        debug "Removing container ${container}"
+        stop_container_sync "${container}"
+    done
+}
+
+# Helper function to execute alertmanager.
+docker_run_alertmanager() {
+    docker run \
+	   --rm \
+	   -d \
+	   --publish ${ALERTMANAGER_PORT}:9093 \
+	   --name "${DOCKER_PREFIX}_${suffix}" \
+	   $DOCKER_IMAGE
+}
+
+wait_alertmanager_container_ready() {
+    local container="${1}"
+    local log="Listening address=:9093"
+    wait_container_ready "${container}" "${log}"
+}
+
+test_fire_an_alert() {
+    debug "Creating alertmanager container"
+    container=$(docker_run_alertmanager)
+    wait_alertmanager_container_ready "${container}" || return 1
+
+    debug "Triggering alert"
+    data=$(cat <<EOF
+{
+  "status": "firing",
+  "labels": {
+    "alertname": "my_testing_alert",
+    "service": "test_service",
+    "severity": "warning",
+    "instance": "fake_instance"
+  },
+  "annotations": {
+    "summary": "This is the summary",
+    "description": "This is the description."
+  },
+  "generatorURL": "https://fake_instance.example/metrics",
+  "startsAt": "2020-08-11T16:00:00+00:00"
+}
+EOF
+)
+
+    debug "Check if the alert was successfully fired"
+    response=$(curl --silent --request POST "http://localhost:${ALERTMANAGER_PORT}/api/v1/alerts" --data "[$data]")
+    assertTrue echo "${response}" | grep success >/dev/null
+}
+
+load_shunit2
