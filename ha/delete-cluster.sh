1fcbcc7eb5025bd8724c5fb76b6baa9fa3629a2f
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2021-04-12 11:24:26 -0300

    ha/delete-cluster.sh: use virsh destroy instead of shutdown
    We do not care about shutting down the nodes cleanly.

--- ha/delete-cluster.sh
+++ ha/delete-cluster.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-virsh shutdown node01
-virsh shutdown node02
-virsh shutdown node03
+virsh destroy node01
+virsh destroy node02
+virsh destroy node03
 
-sleep 20
 virsh undefine node01 --remove-all-storage
 virsh undefine node02 --remove-all-storage
 virsh undefine node03 --remove-all-storage
