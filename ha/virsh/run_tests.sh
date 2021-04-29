d4de94e85c6276f062437a17ece9fb890a2069ad
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2021-04-29 18:48:29 -0300

    ha/virsh/run_tests.sh: setup and delete the cluster between every test
    This is a workaround to have a default cluster without pre-configured
    resources between tests. Some refactoring is needed to avoid recreating
    all the VMs but clean everything up.

--- ha/virsh/run_tests.sh
+++ ha/virsh/run_tests.sh
@@ -4,16 +4,15 @@
 source /etc/profile.d/libvirt-uri.sh
 
 ./delete-cluster.sh || exit 1
-./setup-cluster.sh
 
 test_failed=0
 for file in tests/*_test.sh; do
+  ./setup-cluster.sh
   if ! bash "$file"; then
     test_failed=1
   fi
+  ./delete-cluster.sh
 done
-
-./delete-cluster.sh
 
 if [ $test_failed -eq 1 ]; then
   exit 3
