30d0b1c08825c6bd04152fb8af3c9dec8503b57f
Lucas Kanashiro <lucas.kanashiro@canonical.com>
2021-06-24 16:32:30 -0300

    HA: add iscsi support to setup-cluster.sh
    Now it accepts the --iscsi option which will set up an iscsi target in a
    separate VM and make all the cluster nodes connect to it.

--- ha/virsh/vm_utils.sh
+++ ha/virsh/vm_utils.sh
@@ -2,12 +2,24 @@
 
 UBUNTU_SERIES="${UBUNTU_SERIES:-impish}"
 
+VM_SERVICES="services-${UBUNTU_SERIES}"
+IQN="iqn.$(date '+%Y-%m').com.test.storage"
+
+VM_SERVICES_ISCSI_TARGET="target01"
+VM01_ISCSI_INITIATOR="initiator01"
+VM02_ISCSI_INITIATOR="initiator02"
+VM03_ISCSI_INITIATOR="initiator03"
+
 VM01="fence-test-virsh-${UBUNTU_SERIES}-node01"
 VM02="fence-test-virsh-${UBUNTU_SERIES}-node02"
 VM03="fence-test-virsh-${UBUNTU_SERIES}-node03"
 
 SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
 SCP="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
+
+get_vm_services_ip_address() {
+  IP_VM_SERVICES=$(virsh domifaddr "${VM_SERVICES}" | grep ipv4 | xargs | cut -d ' ' -f4 | cut -d '/' -f1)
+}
 
 get_all_nodes_ip_address() {
   IP_VM01=$(virsh domifaddr "${VM01}" | grep ipv4 | xargs | cut -d ' ' -f4 | cut -d '/' -f1)
