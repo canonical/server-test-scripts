0d36de66a15f109373faef374bd6f9f29298a218
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2023-03-22 09:21:32 +0100

    metric: exclude virtual fs mountpoints from du
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

--- metric-server-simple/metric-server-simple.sh
+++ metric-server-simple/metric-server-simple.sh
@@ -142,7 +142,7 @@ do_measurement_disk() {
 
   # Not parsed (yet), but great for later debugging of differences
   resultfile=$(get_result_filename "diskdetail" "txt")
-  Cexec du  / --max-depth=2 >> "${resultfile}"
+  Cexec du  / --exclude /dev --exclude /proc --exclude /sys --max-depth=2 >> "${resultfile}"
 }
 
 do_measurement_package() {
