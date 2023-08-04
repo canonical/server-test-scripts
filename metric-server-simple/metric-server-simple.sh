canonical/server-test-scripts

commit f444e6e9603d490564a306401fd1e693c6eb59aa

metric-server-simple: ignore RC of systemd-analyze calls
If nonzero we want to continue gathering the rest of the data.

metric-server-simple/metric-server-simple.sh
@@ -140,9 +140,9 @@ do_measurement_servicesecurity() {
   # Check systemd service isolation feature usage
   resultfile=$(get_result_filename "userservicesecurity" "txt")
   # this needs a login session to work
-  Cexec su ubuntu --login -c 'systemd-analyze security --no-pager --user' > "${resultfile}"
+  Cexec su ubuntu --login -c 'systemd-analyze security --no-pager --user' || true > "${resultfile}"
   resultfile=$(get_result_filename "systemservicesecurity" "txt")
-  Cexec systemd-analyze security --no-pager --system > "${resultfile}"
+  Cexec systemd-analyze security --no-pager --system || true > "${resultfile}"
 }
 
 do_measurement_ports() {

commit 66663b8e412510a699cc6c11859d78c31d36772f

metric-server-simple: fix status redirect
The || true recently added did break the redirection which no more
is capturing the output of systemd-analyze. Since `true` has no output
the file isn't created at all and breaks the import.

Change the order so that the output of systemd-analyze is redirected.

Fixes: f444e6e9

Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

metric-server-simple/metric-server-simple.sh
@@ -140,9 +140,9 @@ do_measurement_servicesecurity() {
   # Check systemd service isolation feature usage
   resultfile=$(get_result_filename "userservicesecurity" "txt")
   # this needs a login session to work
-  Cexec su ubuntu --login -c 'systemd-analyze security --no-pager --user' || true > "${resultfile}"
+  Cexec su ubuntu --login -c 'systemd-analyze security --no-pager --user' > "${resultfile}" || true
   resultfile=$(get_result_filename "systemservicesecurity" "txt")
-  Cexec systemd-analyze security --no-pager --system || true > "${resultfile}"
+  Cexec systemd-analyze security --no-pager --system > "${resultfile}" || true
 }
 
 do_measurement_ports() {