canonical/server-test-scripts

commit 7f331de83a886dac55b10f41a316a1767406b2cb

metric-server-simple: gather service status
Sometimes a rare event might happen that is hard to recerate and debug
later. So far we have seen two times that service startup was bad.

Therefore it would be very helpful to have at least the general final
status of all services in the log as it might indicate what was broken.

Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

metric-server-simple/metric-server-simple.sh
@@ -187,6 +187,13 @@ do_install_services() {
       openvpn strongswan
 }
 
+do_log_service_status() {
+    # We sometimes had bad service startup, putting this info to the log
+    # helps to sourt out what might be wrong, especially when not easily
+    # reproducible.
+    systemctl status --all
+}
+
 cleanup
 setup_lxd_minimal_remote
 setup_container
@@ -223,5 +230,7 @@ do_measurement_processcount
 do_measurement_disk
 do_measurement_package
 do_measurement_servicesecurity
+
+do_log_service_status
 
 cleanup

commit 80a5f64ce1be0bd178d5d736ec94347c30616c32

metric-server-simple: ignore RC of combined service status
The RC of systemctl status --all can easily be non-zero while not
being fatal for the system or the metric run. Ignore that to
not run into retries (wasting time) and then giving up eventually.

Fixes: 7f331de8

Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

metric-server-simple/metric-server-simple.sh
@@ -191,7 +191,9 @@ do_log_service_status() {
     # We sometimes had bad service startup, putting this info to the log
     # helps to sourt out what might be wrong, especially when not easily
     # reproducible.
-    systemctl status --all
+    # Ignore the RC which often is 4 meaning that a unit was referenced
+    # but not found e.g. boot.automount in containers
+    systemctl status --all --no-pager || true
 }
 
 cleanup